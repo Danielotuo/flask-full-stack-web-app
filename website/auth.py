c7664ec769266c38543e3682573acf222c7c4f2d
Daniel Otuo-Acheampong <52865422+Danielotuo@users.noreply.github.com>
2021-02-10 17:56:02 -0500

    Added forms and sign up error alerts

--- website/auth.py
+++ website/auth.py
@@ -1,17 +1,37 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstname')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        # show alert if conditions are not met
+        if len(email) < 5:
+            flash('Email must be greater than 4 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 2 characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords do not match.', category='error')
+        elif len(password1) < 8:
+            flash('Password must be greater than 7 characters', category='error')
+        else:
+            # add user to database
+            flash('Account created.', category='success')
     return render_template("sign_up.html")
